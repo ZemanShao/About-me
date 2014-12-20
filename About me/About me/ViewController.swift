91b26fa99c0321b5b54322fe9cac998baf65f355
Nathan <shaozeman@qq.com>
2014-12-20 16:14:05 +0800

    project init

---
+++ About me/About me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  About me
+//
+//  Created by Nathan on 12/20/14.
+//  Copyright (c) 2014 TAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ About me/About meTests/About_meTests.swift
@@ -0,0 +1,36 @@
+//
+//  About_meTests.swift
+//  About meTests
+//
+//  Created by Nathan on 12/20/14.
+//  Copyright (c) 2014 TAC. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class About_meTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
